3e4f0108484b589d254b889edef9767f5ce0e45b
ivan <ivan.stebler@diveria.com>
2025-03-31 18:53:32 -0300

    Add layout and dashboard pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="flex h-20 items-center justify-center bg-gray-100 text-gray-500">
+          Hecho con ❤️ por Vercel
+        </footer>
+      </body>
     </html>
   );
 }
